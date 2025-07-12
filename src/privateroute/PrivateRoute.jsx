79afe5f71617f1b3168ed4a3ceb8d43701c76a1a
Md Hasnath Karim <hasnath.karim@gmail.com>
2025-07-12 14:47:52 +0100

    loading spinner added

--- src/privateroute/PrivateRoute.jsx
+++ src/privateroute/PrivateRoute.jsx
@@ -6,7 +6,13 @@ const PrivateRoute = ({children}) => {
   const { user, loading } = useAuth();
   const location = useLocation();
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-white">
+        <div className="w-12 h-12 border-4 border-red-500 border-dashed rounded-full animate-spin"></div>
+      </div>
+    );
+  }
 
   if (!user) return <Navigate to="/login" state={{ from: location }} replace />;
 
